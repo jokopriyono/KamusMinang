f3ccab523ef25150c731a17b7099f5c24c1472c1
Joko Priyono <joe.mandrake007@gmail.com>
2018-07-30 20:30:47 +0700

    - Implementasi pencarian 3 bahasa menggunakan HashMap Java

---
+++ app/src/main/assets/databases/quotes.sql
@@ -0,0 +1,21 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `quotes` (
+	`quote`	TEXT
+);
+INSERT INTO `quotes` VALUES ('ijsfisjfbv');
+INSERT INTO `quotes` VALUES ('sfvkjsnfk');
+INSERT INTO `quotes` VALUES ('kjsfvsfv');
+INSERT INTO `quotes` VALUES ('jsnfivjns');
+INSERT INTO `quotes` VALUES ('sofjnvsfijn');
+INSERT INTO `quotes` VALUES ('sfvkjsnfjv');
+INSERT INTO `quotes` VALUES ('jsnfvjsnf');
+INSERT INTO `quotes` VALUES ('quote 2');
+INSERT INTO `quotes` VALUES ('quote 3');
+INSERT INTO `quotes` VALUES ('quote 4');
+INSERT INTO `quotes` VALUES ('quote 5');
+INSERT INTO `quotes` VALUES ('quote 6');
+INSERT INTO `quotes` VALUES ('quote 7');
+INSERT INTO `quotes` VALUES ('quote 8');
+INSERT INTO `quotes` VALUES ('quote 9');
+INSERT INTO `quotes` VALUES ('quote 10');
+COMMIT;
--- app/src/main/java/com/kamus/friswells/kamusminang/MainActivity.java
+++ app/src/main/java/com/kamus/friswells/kamusminang/MainActivity.java
@@ -1,10 +1,17 @@
 package com.kamus.friswells.kamusminang;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.kamus.friswells.kamusminang.database.KamusDB;
 
@@ -12,45 +19,87 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressLint("UseSparseArrays")
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ProgressDialog loading;
     private ListView listData;
     private List<String> dataKamus;
     private ArrayAdapter<String> adapter;
+    private Spinner spinBahasa;
+    private EditText edtCari;
+    private HashMap<Integer, String> bahasaMinang = new HashMap<>();
+    private HashMap<Integer, String> bahasaInggris = new HashMap<>();
+    private HashMap<Integer, String> bahasaIndo = new HashMap<>();
+    private Bundle mSavedInstanceState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mSavedInstanceState = savedInstanceState;
 
         listData = findViewById(R.id.listKata);
+        Button btnCari = findViewById(R.id.btn_cari);
+        spinBahasa = findViewById(R.id.spinner);
+        edtCari = findViewById(R.id.edt_search);
+        btnCari.setOnClickListener(this);
 
         munculLoading();
-        new Thread(new Runnable() {
+        spinBahasa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void run() {
-                KamusDB kamusDB = KamusDB.getInstance(MainActivity.this);
-                kamusDB.open();
-                dataKamus = kamusDB.getQuotes();
-                kamusDB.close();
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        hilangLoading();
-                        adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dataKamus);
-                        listData.setAdapter(adapter);
-                    }
-                });
+            public void onItemSelected(AdapterView<?> adapterView, View view, final int pos, long l) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            KamusDB kamusDB = KamusDB.getInstance(MainActivity.this);
+                            kamusDB.open();
+                            switch (pos){
+                                case 0:
+                                    if (mSavedInstanceState==null) {
+                                        bahasaIndo = kamusDB.getBahasaIndo();
+                                        dataKamus = kamusDB.getQuotes();
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dataKamus);
+                                                listData.setAdapter(adapter);
+                                            }
+                                        });
+                                        mSavedInstanceState = new Bundle();
+                                    } else {
+                                        bahasaIndo = kamusDB.getBahasaIndo();
+                                    }
+                                    break;
+                                case 1:
+                                    bahasaMinang = kamusDB.getBahasaMinang();
+                                    break;
+                                default:
+                                    bahasaInggris = kamusDB.getBahasaInggris();
+                                    break;
+                            }
+                            kamusDB.close();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    hilangLoading();
+                                }
+                            });
+                        }
+                    }).start();
             }
-        }).start();
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     private void munculLoading(){
         if (loading==null){
             loading = new ProgressDialog(this);
-            loading.setMessage("Mengambil semua data dari database..");
+            loading.setMessage("Mengambil data dari database..");
             loading.setCancelable(false);
             loading.show();
         }
@@ -60,6 +109,44 @@ public class MainActivity extends AppCompatActivity {
         if (loading!=null){
             loading.dismiss();
             loading = null;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_cari:
+                String cari = edtCari.getText().toString().trim();
+
+                if (spinBahasa.getSelectedItemPosition() == 0){
+                    dataKamus = new ArrayList<>();
+                    for (int key : bahasaIndo.keySet()){
+                        if (bahasaIndo.get(key).contains(cari)){
+                            dataKamus.add(bahasaIndo.get(key));
+                        }
+                    }
+                    adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dataKamus);
+                    listData.setAdapter(adapter);
+                } else if (spinBahasa.getSelectedItemPosition() == 1){
+                    dataKamus = new ArrayList<>();
+                    for (int key : bahasaMinang.keySet()){
+                        if (bahasaMinang.get(key).contains(cari)){
+                            dataKamus.add(bahasaMinang.get(key));
+                        }
+                    }
+                    adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dataKamus);
+                    listData.setAdapter(adapter);
+                } else {
+                    dataKamus = new ArrayList<>();
+                    for (int key : bahasaInggris.keySet()){
+                        if (bahasaInggris.get(key).contains(cari)){
+                            dataKamus.add(bahasaInggris.get(key));
+                        }
+                    }
+                    adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dataKamus);
+                    listData.setAdapter(adapter);
+                }
+                break;
         }
     }
 }
